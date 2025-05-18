e6e30d7c4f90a2b52414561a398c1de6afc8bb6b
liaijun <liaijun.cn@gmail.com>
2025-05-18 23:56:22 +0800

    chore: change dev port

--- vite.config.ts
+++ vite.config.ts
@@ -35,7 +35,7 @@ export default defineConfig(async () => ({
   clearScreen: false,
   // 2. tauri expects a fixed port, fail if that port is not available
   server: {
-    port: 1420,
+    port: 8000,
     strictPort: true,
     host: host || false,
     hmr: host
